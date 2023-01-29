6dcfb067a276a639dbe83158319e5c023524eb75
GUSSCH <steamjg04@gmail.com>
2023-01-29 22:05:14 +0100

    products-sidan försök på js-delen

---
+++ JavaScript/products.js
@@ -0,0 +1,42 @@
+let activeIndex = 0;
+
+const articles = document.querySelector("article");
+
+const GoRight = () => { 
+    const nextIndex = activeIndex + 1 <= articles.length - 1 ? activeIndex + 1 : 0;
+    
+    const currentArticle = document.querySelector(`[data-index="${activeIndex}"]`),
+		nextArticle = document.querySelector(`[data-index="${nextIndex}"]`);
+	
+        //Active group becomes after
+	
+    currentArticle.dataset.status = "after"
+	
+        //Next article element/page
+	nextArticle.dataset.status = "becoming-active-from-after"
+	
+    console.log("RIGHT RIGHT RIGHT");
+
+    setTimeout(() => {
+        nextArticle.dataset.stats = "active";
+        activeIndex = nextIndex;
+    });
+}
+
+const GoLeft = () => {
+    const nextIndex = activeIndex - 1 >= 0? activeIndex - 1 : articles.length - 1;
+    
+    const currentArticle = document.querySelector(`[data-index="${activeIndex}"]`),
+    nextArticle = document.querySelector(`[data-index="$(nextIndex)"]`);
+    
+    
+    currentArticle.dataset.status = "before";     
+    nextArticle.dataset.status = "becoming-active-from-after";
+
+    console.log("LEFT LEFT LEFT");
+
+    setTimeout(() => {
+        nextArticle.dataset.stats = "active";
+        activeIndex = nextIndex;
+    });
+}
