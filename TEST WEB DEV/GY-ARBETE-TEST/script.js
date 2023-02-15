deba8e6b8607057a3f11bac6ec397fc8c4bc6fd0
GUSSCH <steamjg04@gmail.com>
2023-02-15 14:09:02 +0100

    slide-funktion fixad med arrows

--- TEST WEB DEV/GY-ARBETE-TEST/script.js
+++ TEST WEB DEV/GY-ARBETE-TEST/script.js
@@ -7,14 +7,14 @@ let nextIndex = 0;
 const GoLeft = () => {
     const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : articles.length - 1;
 
-
     const currentArticle = document.querySelector(`[data-index="${activeIndex}"]`),
-    nextArticle = document.querySelector(`[data-index="$(nextIndex)"]`);
+    nextArticle = document.querySelector(`[data-index="${nextIndex}"]`);
     
-    
+    console.log(nextArticle);
+
     currentArticle.dataset.status = "before";     
     nextArticle.dataset.status = "becoming-active-from-after";
-
+    
     console.log("LEFT LEFT LEFT");
 
     setTimeout(() => {
@@ -26,13 +26,11 @@ const GoRight = () => {
     const nextIndex = activeIndex + 1 <= articles.length - 1 ? activeIndex + 1 : 0;
                             
     const currentArticle = document.querySelector(`[data-index="${activeIndex}"]`),
-		nextArticle = document.querySelector(`[data-index="${nextIndex}"]`);
-	
-        //Active group becomes after
+	nextArticle = document.querySelector(`[data-index="${nextIndex}"]`);
+
+    console.log(nextArticle);
 	
     currentArticle.dataset.status = "after"
-	
-        //Next article element/page
 	nextArticle.dataset.status = "becoming-active-from-before"
     
     console.log("RIGHT RIGHT RIGHT");
