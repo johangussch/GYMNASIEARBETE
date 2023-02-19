e452ef1b177374b4a0d9afe35ca241c64de2164b
GUSSCH <steamjg04@gmail.com>
2023-02-19 20:11:34 +0100

    home sida forts. + la till html för avdelningarna

---
+++ JavaScript/account.js
@@ -0,0 +1,7 @@
+// POP UP DROP DOWN LIST WITH LOGIN INFORMATION
+const account = () => {
+    const account = document.querySelector(".nav-account");
+    account.classList.toggle("account-active");
+
+    // LÄGG TILL KLASSEN ACCOUNT-ACTIVE PÅ VARJE CSS FIL OCH STYLA EXAKT LIKADANT PÅ VARJE
+}
