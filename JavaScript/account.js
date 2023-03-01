a4f723b65f6cd3b47e51de6756b69451657878d8
GUSSCH <steamjg04@gmail.com>
2023-03-01 21:36:02 +0100

    account popup on home

--- JavaScript/account.js
+++ JavaScript/account.js
@@ -1,7 +1,12 @@
 // POP UP DROP DOWN LIST WITH LOGIN INFORMATION
 const account = () => {
-    const account = document.querySelector(".nav-account");
-    account.classList.toggle("account-active");
-
-    // LÄGG TILL KLASSEN ACCOUNT-ACTIVE PÅ VARJE CSS FIL OCH STYLA EXAKT LIKADANT PÅ VARJE
+    let popup = document.querySelector(".account-popup");
+    
+    if (popup.style.display === "none") {
+        popup.style.display = "block";
+    } else {
+        popup.style.display = "none";
+    }
 }
+
+// LÄGG TILL KLASSEN ACCOUNT-ACTIVE PÅ VARJE CSS FIL OCH STYLA EXAKT LIKADANT PÅ VARJE
--- PHP/login.php
+++ PHP/login.php
@@ -1,3 +1,16 @@
 <?php
+session_start();
 
+// Check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get the username from the form
+  $username = $_POST["Username"];
+  
+  // Store the username in a session variable
+  $_SESSION["username"] = $username;
+  
+  // Redirect the user to home.html
+  header("Location: HTML/home.html");
+  exit;
+}
 ?>
