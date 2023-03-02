519144e85dd3144f575d68f0c0cef50b0a5b8821
GUSSCH <steamjg04@gmail.com>
2023-03-03 00:10:51 +0100

    login func. + removed account btn on products

--- JavaScript/account.js
+++ JavaScript/account.js
@@ -1,43 +1,27 @@
-// POP UP DROP DOWN LIST WITH LOGIN INFORMATION
-const account = () => {
+function account() {
   let popup = document.querySelector(".account-popup");
-
-  if (popup.style.display === "none") {
-    popup.style.display = "block";
-  } else {
+  
+  if (popup.style.display === "block") {
     popup.style.display = "none";
+  } else {
+    popup.style.display = "block";
   }
 };
 
-// LÄGG TILL KLASSEN ACCOUNT-ACTIVE PÅ VARJE CSS FIL OCH STYLA EXAKT LIKADANT PÅ VARJE
+const isLoggedIn = localStorage.getItem("isLoggedIn");
+function login() {
+  localStorage.setItem("isLoggedIn", true);
+}
 
-const express = require("express");
-const session = require("express-session");
-const app = express();
+if (isLoggedIn) {
+  document.querySelector(".login-switch").style.display = "none";
+  
+  document.querySelector(".temp-logout").style.display = "block";
+  
+  document.querySelector(".account-status").textContent = "You are logged in.";
 
-// set up the session middleware
-app.use(
-  session({
-    secret: "mySecretKey",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-
-// route for handling login
-app.post("/login", (req, res) => {
-  // set the session variable when user logs in
-  req.session.username = "JohnDoe";
-  res.send("Logged in successfully!");
-});
-
-// route for displaying the account popup
-app.get("/account", (req, res) => {
-  const username = req.session.username || "Guest";
-  const popupContent = `You are now logged in as ${username}`;
-  res.send(popupContent);
-});
-
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+  document.querySelector(".temp-logout").onclick = function() {
+    localStorage.clear();
+    location.reload();
+  }
+}
