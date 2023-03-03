adfc0db5958a78cc9a42923e51832248c9ed0d39
GUSSCH <steamjg04@gmail.com>
2023-03-04 00:09:57 +0100

    fixed price funcs.

--- JavaScript/cartAdd.js
+++ JavaScript/cartAdd.js
@@ -6,7 +6,9 @@ let cartQuantity2 = document.querySelector("#counter-cart-2");
 let cartQuantity3 = document.querySelector("#counter-cart-3");
 
 
-let price = document.querySelector(".price");
+let price1 = document.querySelector(".price-1");
+let price2 = document.querySelector(".price-2");
+let price3 = document.querySelector(".price-3");
 
 let totalPrice = 0;
 
@@ -14,7 +16,7 @@ document.querySelector(".add-1").addEventListener("click", function () {
   cartQuantity1.innerHTML = 0;
   click1 += 1;
   cartQuantity1.innerHTML = click1;
-  totalPrice += parseFloat(price.textContent);
+  totalPrice += parseFloat(price1.textContent);
   document.querySelector(".total-text").innerHTML = totalPrice;
 });
 
@@ -22,7 +24,7 @@ document.querySelector(".add-2").addEventListener("click", function () {
   cartQuantity2.innerHTML = 0;
   click2 += 1;
   cartQuantity2.innerHTML = click2;
-  totalPrice += parseFloat(price.textContent);
+  totalPrice += parseFloat(price2.textContent);
   document.querySelector(".total-text").innerHTML = totalPrice;
 });
 
@@ -30,7 +32,7 @@ document.querySelector(".add-3").addEventListener("click", function () {
   cartQuantity3.innerHTML = 0;
   click3 += 1;
   cartQuantity3.innerHTML = click3;
-  totalPrice += parseFloat(price.textContent);
+  totalPrice += parseFloat(price3.textContent);
   document.querySelector(".total-text").innerHTML = totalPrice;
 });
 
@@ -39,7 +41,7 @@ document.querySelector(".add-3").addEventListener("click", function () {
 document.querySelector(".remove-1").addEventListener("click", function () {
   if (click1 > 0) {
     cartQuantity1.innerHTML = --click1;
-    totalPrice -= parseFloat(price.textContent);
+    totalPrice -= parseFloat(price1.textContent);
     document.querySelector(".total-text").innerHTML = Math.max(totalPrice, 0);
   }
 });
@@ -47,7 +49,7 @@ document.querySelector(".remove-1").addEventListener("click", function () {
 document.querySelector(".remove-2").addEventListener("click", function () {
   if (click2 > 0) {
     cartQuantity2.innerHTML = --click2;
-    totalPrice -= parseFloat(price.textContent);
+    totalPrice -= parseFloat(price2.textContent);
     document.querySelector(".total-text").innerHTML = Math.max(totalPrice, 0);
   }
 });
@@ -55,7 +57,7 @@ document.querySelector(".remove-2").addEventListener("click", function () {
 document.querySelector(".remove-3").addEventListener("click", function () {
   if (click3 > 0) {
     cartQuantity3.innerHTML = --click3;
-    totalPrice -= parseFloat(price.textContent);
+    totalPrice -= parseFloat(price3.textContent);
     document.querySelector(".total-text").innerHTML = Math.max(totalPrice, 0);
   }
 });
